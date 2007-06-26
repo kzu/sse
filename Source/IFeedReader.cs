33afc38c0a7237c7dbcba3e82e612b7bf79c714a
Daniel Cazzulino <daniel@cazzulino.com>
2007-06-26 23:56:19 +0000

    Initial release

---
+++ Source/IFeedReader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleSharing
+{
+	public interface IFeedReader
+	{
+		event EventHandler ItemRead;
+		void Read(out Feed feed, out IEnumerable<Item> items);
+	}
+}
