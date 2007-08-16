b2d09c3c70f391b1069b7a7b82743d55fffc4343
dcazzulino_cp <SND\dcazzulino_cp>
2007-08-16 02:08:21 +0000

    Added global assembly information file. Fixed copyright info.

---
+++ Main/GlobalAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("MVPXML Project")]
+[assembly: AssemblyCopyright("Copyright ©  2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.93.5.0")]
+[assembly: AssemblyFileVersion("0.93.5.0")]
--- Main/Source/Properties/AssemblyInfo.cs
+++ Main/Source/Properties/AssemblyInfo.cs
@@ -1,34 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("SimpleSharing")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MVPXML Implementation of the SSE standard")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyProduct("SimpleSharing")]
-[assembly: AssemblyCopyright("Microsoft Corporation")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dd9dc848-d72c-49b2-b5ca-cf2861f94f93")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("0.93.1.0")]
