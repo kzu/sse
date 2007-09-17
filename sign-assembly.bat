42c2416e9f41f0d1d920e14f3f4b919cf58e56c1
dcazzulino_cp <SND\dcazzulino_cp>
2007-09-17 19:13:37 +0000

    More fixes for branches.

--- sign-assembly.bat
+++ sign-assembly.bat
@@ -1,5 +1,5 @@
 @echo Usage: sign MyAssembly[dll or exe]
 
 @echo off
-sn -Ra %1 Mvp-Xml.Net20.snk
-signcode -spc xmlmvpcert.spc -v xmlmvpkey.pvk -t http://timestamp.verisign.com/scripts/timstamp.dll %1
+sn -Ra %1 %~p0\Mvp-Xml.Net20.snk
+%~p0\signcode -spc %~p0\xmlmvpcert.spc -v %~p0\xmlmvpkey.pvk -t http://timestamp.verisign.com/scripts/timstamp.dll %1
