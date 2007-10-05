efb145c8beb85300765b749c83429bce5b83b9d7
Daniel Cazzulino <daniel@cazzulino.com>
2007-10-05 20:27:34 +0000

    Cleanup.

--- Adapters/Memory/Source/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("SimpleSharing.Adapters.Memory")]
-[assembly: AssemblyDescription("XMLMVP SSE Memory Adapter")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("SimpleSharing.Adapters.Memory")]
