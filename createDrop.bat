c422fe31dc693aca749438a1b04defd915d32361
dcazzulino_cp <SND\dcazzulino_cp>
2007-10-05 19:06:54 +0000

    updated script and signed the new assemblies.

--- createDrop.bat
+++ createDrop.bat
@@ -1,7 +1,15 @@
+msbuild /p:Configuration=Release Main\SimpleSharing.sln
+
 xcopy .\Main\Source\bin\Release\SimpleSharing.* .\Drops\Current\ /F /Y /R
-xcopy .\Main\Source\bin\Release\CF\SimpleSharing.CF.* .\Drops\Current\CF\ /F /Y /R
+xcopy .\Main\Source\bin\Release\CF\SimpleSharing.CF.* .\Drops\Current\ /F /Y /R
 xcopy .\Main\UnitTests\bin\Release\SimpleSharing.Tests.* .\Drops\Current\ /F /Y /R
 
+xcopy .\Main\Adapters\SimpleSharing.Data\Source\bin\Release\SimpleSharing.Data.* .\Drops\Current\ /F /Y /R
+xcopy .\Main\Adapters\SimpleSharing.Data\Source.CF\bin\Release\SimpleSharing.Data.CF.* .\Drops\Current\ /F /Y /R
+
 call %~p0sign-assembly.bat .\Drops\Current\SimpleSharing.dll %1
-call %~p0sign-assembly.bat .\Drops\Current\CF\SimpleSharing.CF.dll %1
+call %~p0sign-assembly.bat .\Drops\Current\SimpleSharing.CF.dll %1
 call %~p0sign-assembly.bat .\Drops\Current\SimpleSharing.Tests.dll %1
+
+call %~p0sign-assembly.bat .\Drops\Current\SimpleSharing.Data.dll %1
+call %~p0sign-assembly.bat .\Drops\Current\SimpleSharing.Data.CF.dll %1
