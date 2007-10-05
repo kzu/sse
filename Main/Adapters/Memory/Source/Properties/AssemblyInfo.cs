9e382e3581dbb59250331256d53ba4f31cb36f1a
dcazzulino_cp <SND\dcazzulino_cp>
2007-10-05 20:26:11 +0000

    Added in-memory repository used mostly for testing purposes.

---
+++ Main/Adapters/Memory/Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SimpleSharing.Adapters.Memory")]
+[assembly: AssemblyDescription("XMLMVP SSE Memory Adapter")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("SimpleSharing.Adapters.Memory")]
