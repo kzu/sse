77edbd73b0c077fd990386199aeafc366f3d5832
Daniel Cazzulino <daniel@cazzulino.com>
2007-10-05 19:26:58 +0000

    Fixed global info.

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -2,10 +2,12 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCompany("MVPXML Project")]
+[assembly: AssemblyCompany("XMLMVP Project")]
 [assembly: AssemblyCopyright("Copyright ©  2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.94.0.0")]
-[assembly: AssemblyFileVersion("0.94.0.0")]
+[assembly: AssemblyVersion("0.94.1.*")]
+#if !PocketPC
+[assembly: AssemblyFileVersion("0.94.1.*")]
+#endif
