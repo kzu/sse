6b5c3c51bda887ce619d83a9a28b8b600f2e305d
pcibraro_cp <SND\pcibraro_cp>
2008-04-29 18:28:15 +0000

    Updated assembly version

--- Main/GlobalAssemblyInfo.cs
+++ Main/GlobalAssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
 #if !PocketPC
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 #endif
